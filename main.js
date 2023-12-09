e8afab28e8b998133eea18694fc2fae17515e027
AungKoMin93 <aungkomin@gusto.edu.com.mm>
2023-12-09 23:39:30 +0630

    Ten mini js  project

---
+++ main.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
+
+
+document.querySelector('body').addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+})
